b363dc607692af4688e94f5a04e15147f7355e24
tinywind <tinywind0@gmail.com>
2021-08-29 01:10:40 +0900

    add markdown app

--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="my-auto">
-    <h1 class="text-center">Markdown</h1>
+  <div class="flex flex-wrap w-full">
+    <h1 class="w-full text-center text-3xl my-4">Markdown App</h1>
+    <section class="flex m-auto w-10/12 h-screen">
+      <article class="w-1/2 border">
+        <textarea class="w-full h-full" @input="update"></textarea>
+      </article>
+      <article class="w-1/2 border bg-gray-100" v-html="getMarked"/>
+    </section>
   </div>
 </template>
-
 <script>
+import marked from 'marked'
+import debounce from "@/utillities/debounce";
+
 export default {
-  name: "Markdown"
+  name: "Markdown",
+  mixins: [debounce],
+  data() {
+    return {
+      text: '',
+    }
+  },
+  computed: {
+    getMarked() {
+      return marked(this.text)
+    }
+  },
+  methods: {
+    update(e) {
+      this.debounce(() => this.text = e.target.value)
+    },
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
